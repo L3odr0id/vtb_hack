9b51110f46533cff55c7964ab690a2603e92581c
kesler <kd7614@yandex.ri>
2021-10-09 20:28:59 +0300

    safe area and correct button

--- lib/presentation/pages/game/choose_strategy.dart
+++ lib/presentation/pages/game/choose_strategy.dart
@@ -23,111 +23,113 @@ class _ChooseStrategyState extends State<ChooseStrategy> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xff295CA9),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          Padding(
-            padding: const EdgeInsets.fromLTRB(15, 20, 15, 14),
-            child: Card(
-              child: Container(
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.white),
-                height: 230,
-                child: Stack(
-                  children: [
-                    Column(
-                      children: [
-                        Row(
-                          children: [
-                            Flexible(
-                              child: Padding(
-                                padding:
-                                const EdgeInsets.fromLTRB(20, 16, 20, 10),
-                                child: Text(
-                                  "Какую стратегию ты хочешь выбрать?",
-                                  style: GoogleFonts.roboto(
-                                      textStyle: TextStyle(
-                                          fontWeight: FontWeight.w500,
-                                          fontSize: 24,
-                                          color: Colors.black)),
-                                ),
-                              ),
-                            )
-                          ],
-                        ),
-                        ListTile(
-                          title: Text(
-                            'Долгосрочное \nинвестирование',
-                            style: GoogleFonts.roboto(textStyle: _textStyle()),
-                          ),
-                          leading: Radio<Strategy>(
-                            value: Strategy.longInves,
-                            groupValue: _strategy,
-                            onChanged: (Strategy? value) {
-                              setState(() {
-                                _strategy = value;
-                              });
-                            },
-                          ),
-                        ),
-                        ListTile(
-                          title: Text(
-                            'Краткосрочное \nинвестирование',
-                            style: _textStyle(),
-                          ),
-                          leading: Radio<Strategy>(
-                            value: Strategy.shortInvest,
-                            groupValue: _strategy,
-                            onChanged: (Strategy? value) {
-                              setState(() {
-                                _strategy = value;
-                              });
-                            },
-                          ),
-                        ),
-                      ],
-                    ),
-
-                    Padding(
-                      padding: const EdgeInsets.only(top: 111),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(15, 20, 15, 0),
+              child: Card(
+                child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.white),
+                  height: 230,
+                  child: Stack(
+                    children: [
+                      Column(
                         children: [
-                          RotatedBox(
-                            quarterTurns: 2,
-                            child: Image(image: AssetImage('assets/other/25.png'),
-                              height: 124,
-                              alignment: Alignment.topRight,),
+                          Row(
+                            children: [
+                              Flexible(
+                                child: Padding(
+                                  padding:
+                                  const EdgeInsets.fromLTRB(20, 16, 20, 10),
+                                  child: Text(
+                                    "Какую стратегию ты хочешь выбрать?",
+                                    style: GoogleFonts.roboto(
+                                        textStyle: TextStyle(
+                                            fontWeight: FontWeight.w500,
+                                            fontSize: 24,
+                                            color: Colors.black)),
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                          ListTile(
+                            title: Text(
+                              'Долгосрочное \nинвестирование',
+                              style: GoogleFonts.roboto(textStyle: _textStyle()),
+                            ),
+                            leading: Radio<Strategy>(
+                              value: Strategy.longInves,
+                              groupValue: _strategy,
+                              onChanged: (Strategy? value) {
+                                setState(() {
+                                  _strategy = value;
+                                });
+                              },
+                            ),
+                          ),
+                          ListTile(
+                            title: Text(
+                              'Краткосрочное \nинвестирование',
+                              style: _textStyle(),
+                            ),
+                            leading: Radio<Strategy>(
+                              value: Strategy.shortInvest,
+                              groupValue: _strategy,
+                              onChanged: (Strategy? value) {
+                                setState(() {
+                                  _strategy = value;
+                                });
+                              },
+                            ),
                           ),
                         ],
                       ),
-                    ),
-                  ],
+
+                      Padding(
+                        padding: const EdgeInsets.only(top: 111),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            RotatedBox(
+                              quarterTurns: 2,
+                              child: Image(image: AssetImage('assets/other/25.png'),
+                                height: 124,
+                                alignment: Alignment.topRight,),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-          Stack(
-            children: [
-              Image(image: AssetImage('assets/instruments/tuan_1.png')),
-              Padding(
-                padding: const EdgeInsets.only(left: 15),
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.white,
+            Stack(
+              children: [
+                Image(image: AssetImage('assets/instruments/tuan_1.png')),
+                Padding(
+                  padding: const EdgeInsets.only(left: 19),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.white,
+                    ),
+                    onPressed: () {},
+                    child: Text("Далее",
+                        style: GoogleFonts.roboto(
+                          fontWeight: FontWeight.w400,
+                          color: Color(0xff3A83F1),
+                        )),
                   ),
-                  onPressed: () {},
-                  child: Text("Далее",
-                      style: GoogleFonts.roboto(
-                        fontWeight: FontWeight.w400,
-                        color: Color(0xff3A83F1),
-                      )),
-                ),
-              )
-            ],
-          ),
-        ],
+                )
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
