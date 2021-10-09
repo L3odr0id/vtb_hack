26737ac484cebfe853c0b6ecfce370ef1390961e
L3odr0id <leon.leonc@yandex.ru>
2021-10-09 17:55:57 +0300

    Resources

--- lib/common/constants.dart
+++ lib/common/constants.dart
@@ -4,6 +4,8 @@ import 'package:google_fonts/google_fonts.dart';
 class GetColor {
   static const Color darkBlueVTB = Color(0xff02298C);
   static const Color lightCircle = Color(0xff00A5FF);
+  static const Color dark2 = Color(0xff295CA9);
+  static const Color mainVTB = Color(0xff3A83F1);
 }
 
 class GetTextStyle {
@@ -11,4 +13,10 @@ class GetTextStyle {
       fontWeight: FontWeight.w400,
       fontSize: 10,
       color: Color.fromARGB(204, 47, 52, 65));
+
+  static TextStyle header = GoogleFonts.roboto(
+      fontWeight: FontWeight.w500, fontSize: 24, color: Colors.white);
+
+  static TextStyle desc = GoogleFonts.roboto(
+      fontWeight: FontWeight.w300, fontSize: 18, color: Colors.white);
 }
