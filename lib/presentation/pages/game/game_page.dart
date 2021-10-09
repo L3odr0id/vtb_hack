53f021dee52132b66279e56548d17244d5a1c2bd
kesler <kd7614@yandex.ri>
2021-10-09 15:34:41 +0300

    bill and risk fields

--- lib/presentation/pages/game/game_page.dart
+++ lib/presentation/pages/game/game_page.dart
@@ -25,7 +25,8 @@ class _GamePageState extends State<GamePage> {
     return Scaffold(
         body: SafeArea(
       child: Column(children: [
-        _indicators(),
+        _bill(),
+        _risk(),
         _text(),
         _cards(),
         _stats(),
@@ -33,27 +34,114 @@ class _GamePageState extends State<GamePage> {
     ));
   }
 
-  Widget _indicators() {
-    return CircularPercentIndicator(
-      radius: 160.0,
-      animation: true,
-      animationDuration: 1200,
-      lineWidth: 20.0,
-      percent: 0.5,
-      center: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+  Widget _bill() {
+    return Column(
+        mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          Text(
-            'общий капитал',
-            style: GetTextStyle.smallGrey,
+          Padding(
+            padding: const EdgeInsets.fromLTRB(20, 18, 0, 0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(
+                  "На счету",
+                  textAlign: TextAlign.left,
+                  style: GoogleFonts.roboto(
+                    textStyle: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w300,
+                        color: Colors.black),
+                  ),
+                ),
+              ],
+            ),
           ),
-          Text(currentState.currentMoney.toString())
+          Padding(
+            padding: const EdgeInsets.fromLTRB(15, 10, 0, 0),
+            child: Row(
+              children: [
+                Text(
+                  "1.000.000 ",
+                  style: GoogleFonts.roboto(
+                    textStyle: TextStyle(
+                        fontSize: 36,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.black),
+                  ),
+                ),
+                Container(
+                  width: 94,
+                  height: 23,
+                  margin: EdgeInsets.only(left: 26.0),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Color(0xffD8E6FC)),
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(8, 4, 0, 4),
+                        child: Icon(
+                          Icons.arrow_downward_sharp,
+                          color: Color(0xffFF4459),
+                          size: 15,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(10, 3, 8, 4),
+                        child: Text(
+                          "23.234",
+                          style: GoogleFonts.roboto(
+                            textStyle: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w400,
+                                color: Color(0xffFF4459)),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
         ],
-      ),
-      circularStrokeCap: CircularStrokeCap.butt,
-      // backgroundColor: GetColor.blueVTB,
-      progressColor: GetColor.lightCircle,
-    );
+      );
+  }
+
+  Widget _risk() {
+    return Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+        Padding(
+          padding: const EdgeInsets.fromLTRB(20, 0, 0, 0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(
+                "Риск",
+                textAlign: TextAlign.left,
+                style: GoogleFonts.roboto(
+                  textStyle: TextStyle(
+                      fontSize: 18, fontWeight: FontWeight.w300, color: Colors.black),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(20, 10, 0, 0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text("89%",
+                  style: GoogleFonts.roboto(
+                    textStyle: TextStyle(
+                        fontSize: 36,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.black),
+                  )),
+            ],
+          ),
+        ),
+      ]);
   }
 
   Widget _text() {
@@ -63,7 +151,8 @@ class _GamePageState extends State<GamePage> {
         child: Container(
           height: 246,
           decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10), color: Color(0xff3A83F1)),
+              borderRadius: BorderRadius.circular(10),
+              color: Color(0xff3A83F1)),
           child: Column(children: [
             Padding(
               padding: const EdgeInsets.fromLTRB(0, 12, 0, 5),
