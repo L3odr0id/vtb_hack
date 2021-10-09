ff59802f39b4b8ee31298b54086f2a540879b79b
L3odr0id <leon.leonc@yandex.ru>
2021-10-09 19:49:02 +0300

    safe area fix

--- lib/presentation/pages/home/home_page.dart
+++ lib/presentation/pages/home/home_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/rendering.dart';
 import 'package:vtb_game_win/common/constants.dart';
 import 'package:vtb_game_win/presentation/pages/about/about_page.dart';
 import 'package:vtb_game_win/presentation/pages/fame/hall_of_fame.dart';
+import 'package:vtb_game_win/presentation/pages/game/choose_strategy.dart';
 import 'package:vtb_game_win/presentation/pages/game/game_page.dart';
 
 class HomePage extends StatefulWidget {
@@ -12,7 +13,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int _selectedIndex = 1;
-  Widget _hallOfFame = HallOfFamePage();
+  Widget _hallOfFame = ChooseStrategy();
   Widget _game = GamePage();
   // Widget _about = AboutPage();
 
@@ -54,6 +55,8 @@ class _HomePageState extends State<HomePage> {
       case 0:
         return this._hallOfFame;
       case 1:
+        return this._hallOfFame;
+      case 2:
         return this._game;
       // case 2:
       //   return this._about;
