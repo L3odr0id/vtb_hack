eeb09001a3b6737467fcf1c6abf3e3e8694fd643
L3odr0id <leon.leonc@yandex.ru>
2021-10-09 09:59:08 +0300

    Navigation

---
+++ lib/common/constants.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+Color blueVTB = Color(0xff02298C);
--- lib/presentation/pages/home/home_page.dart
+++ lib/presentation/pages/home/home_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:vtb_game_win/common/constants.dart';
+import 'package:vtb_game_win/presentation/pages/about/about_page.dart';
+import 'package:vtb_game_win/presentation/pages/fame/hall_of_fame.dart';
 import 'package:vtb_game_win/presentation/pages/game/game_page.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,17 +11,65 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 1;
+  Widget _hallOfFame = HallOfFamePage();
+  Widget _game = GamePage();
+  Widget _about = AboutPage();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: ElevatedButton(
-        child: Text('Играть'),
-        onPressed: () {
-          Navigator.of(context)
-              .push(MaterialPageRoute(builder: (context) => GamePage()));
+      body: getBody(),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: blueVTB,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        type: BottomNavigationBarType.fixed,
+        currentIndex: this._selectedIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: _bottomBarIcon('trophy'),
+            label: "Hall of fame",
+          ),
+          BottomNavigationBarItem(icon: _bottomBarIcon('home'), label: "Game"),
+          BottomNavigationBarItem(
+            icon: _bottomBarIcon('profile'),
+            label: "About",
+          )
+        ],
+        onTap: (int index) {
+          this.onTapHandler(index);
         },
       ),
-    ));
+    );
+  }
+
+  Widget getBody() {
+    switch (this._selectedIndex) {
+      case 0:
+        return this._hallOfFame;
+      case 1:
+        return this._game;
+      case 2:
+        return this._about;
+      default:
+        return this._game;
+    }
+  }
+
+  void onTapHandler(int index) {
+    this.setState(() {
+      this._selectedIndex = index;
+    });
+  }
+
+  Widget _bottomBarIcon(String image) {
+    return Container(
+        height: 25,
+        width: 25,
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                fit: BoxFit.contain,
+                image: AssetImage('assets/icons/' + image + '.png'))));
   }
 }
